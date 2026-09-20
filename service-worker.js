b8aa2a46919c028be6c9a5e9f001d7f8015b8495
ericlagunas84-cmyk <eric.lagunas84@gmail.com>
2026-09-20 01:06:43 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // siempre intenta traer la versión más reciente, y solo recurre a la
 // copia en caché si no hay conexión a internet.
 
-const CACHE_NAME = 'gael-v20';
+const CACHE_NAME = 'gael-v21';
 
 const PRECACHE_URLS = [
   './',
